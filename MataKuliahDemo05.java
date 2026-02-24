c427b2f98b3c5e053b49025857f7117017a21ce1
ranran-beib <254107060060@student.polinema.ac.id>
2026-02-24 12:18:27 +0700

    Modifikasi P3: Constructor Berparameter

--- MataKuliahDemo05.java
+++ MataKuliahDemo05.java
@@ -22,8 +22,15 @@ public class MataKuliahDemo05 {
             System.out.println ("=====================================");
 
             arrayOfMataKuliah[i] = new MataKuliah05 (kode, nama, sks, jumlahJam);
+        }
 
-            
+        for (int i = 0; i < 3; i++) {
+            System.out.println ("Data Mata Kuliah ke-" + (i + 1));
+            System.out.println ("Kode         : " + arrayOfMataKuliah[i].kode);
+            System.out.println ("Nama         : " + arrayOfMataKuliah[i].nama);
+            System.out.println ("SKS          : " + arrayOfMataKuliah[i].sks);
+            System.out.println ("Jumlah Jam   : " + arrayOfMataKuliah[i].jumlahJam);
+            System.out.println ("=====================================");
         }
 
     }
