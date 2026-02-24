f4f5913cbdd1d750858348633af57304343e7254
ranran-beib <254107060060@student.polinema.ac.id>
2026-02-24 14:27:09 +0700

    Modifikasi P3: Constructor Berparameter

--- MataKuliah05.java
+++ MataKuliah05.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 public class MataKuliah05 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
 
-    public MataKuliah05 (String kode, String nama, int sks, int jumlahJam) {
+    public MataKuliah05() {
+    }
+
+    public MataKuliah05(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData(Scanner sc) {
+        System.out.print("Kode         : ");
+        kode = sc.nextLine();
+        System.out.print("Nama         : ");
+        nama = sc.nextLine();
+        System.out.print("SKS          : ");
+        sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah Jam   : ");
+        jumlahJam = Integer.parseInt(sc.nextLine());
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode         : " + kode);
+        System.out.println("Nama         : " + nama);
+        System.out.println("SKS          : " + sks);
+        System.out.println("Jumlah Jam   : " + jumlahJam);
+        System.out.println("=====================================");
     }
 }
--- MataKuliahDemo05.java
+++ MataKuliahDemo05.java
@@ -2,36 +2,28 @@ import java.util.Scanner;
 
 public class MataKuliahDemo05 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        MataKuliah05[] arrayOfMataKuliah = new MataKuliah05[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println ("Masukkan Data Mata Kuliah ke-" + (i + 1));
-            System.out.print ("Kode         : ");
-            kode = sc.nextLine();
-            System.out.print ("Nama         : ");
-            nama = sc.nextLine();
-            System.out.print ("SKS          : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print ("Jumlah Jam   : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println ("=====================================");
+        // User menentukan jumlah array
+        System.out.print("Masukkan jumlah Mata Kuliah: ");
+        int jumlah = Integer.parseInt(sc.nextLine());
 
-            arrayOfMataKuliah[i] = new MataKuliah05 (kode, nama, sks, jumlahJam);
+        MataKuliah05[] arrayOfMataKuliah = new MataKuliah05[jumlah];
+
+        // Input data
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nMasukkan Data Mata Kuliah ke-" + (i + 1));
+            arrayOfMataKuliah[i] = new MataKuliah05();
+            arrayOfMataKuliah[i].tambahData(sc);
         }
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println ("Data Mata Kuliah ke-" + (i + 1));
-            System.out.println ("Kode         : " + arrayOfMataKuliah[i].kode);
-            System.out.println ("Nama         : " + arrayOfMataKuliah[i].nama);
-            System.out.println ("SKS          : " + arrayOfMataKuliah[i].sks);
-            System.out.println ("Jumlah Jam   : " + arrayOfMataKuliah[i].jumlahJam);
-            System.out.println ("=====================================");
+        // Output data
+        System.out.println("\n===== DATA MATA KULIAH =====");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Mata Kuliah ke-" + (i + 1));
+            arrayOfMataKuliah[i].cetakInfo();
         }
 
+        sc.close();
     }
 }
